f26ea6ef75d4c90d4302a3fc372743a9d52f645d
Tomas Sapak <139890@muni.cz>
2023-09-15 15:29:29 +0200

    Fix syntax

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ -z "$DBNAME" ]
 then
     exec "$@"
 else
-    PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "CREATE USER \"$USER\";" 2> /dev/null
-    PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "ALTER USER \"$USER\" PASSWORD \"$PASSWORD\";" 2> /dev/null
-    PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "CREATE DATABASE \"$DBNAME\" OWNER \"$USER\";" 2> /dev/null
+    PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "CREATE USER \"$USER\";"
+    PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "ALTER USER \"$USER\" PASSWORD '$PASSWORD';"
+    PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "CREATE DATABASE \"$DBNAME\" OWNER \"$USER\";"
 fi
