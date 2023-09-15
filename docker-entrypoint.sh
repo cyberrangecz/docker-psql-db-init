af024dff413923ce436448e7f4a8097d7ccb6b0d
Tomas Sapak <139890@muni.cz>
2023-09-15 15:24:03 +0200

    Fix database name variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,5 +7,5 @@ then
 else
     PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "CREATE USER \"$USER\";" 2> /dev/null
     PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "ALTER USER \"$USER\" PASSWORD \"$PASSWORD\";" 2> /dev/null
-    PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "CREATE DATABASE \"$DATABASE\" OWNER \"$USER\";" 2> /dev/null
+    PGPASSWORD="$ADMIN_PASSWORD" psql -h "$HOST" -p "$PORT" -U "$ADMIN_USER" -c "CREATE DATABASE \"$DBNAME\" OWNER \"$USER\";" 2> /dev/null
 fi
